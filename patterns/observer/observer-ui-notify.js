361de796b12cadae867b56bc5a63cc3eb482f0a5
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-03-08 12:53:47 +0100

    [observer] ui notifications

---
+++ patterns/observer/observer-ui-notify.js
@@ -0,0 +1,35 @@
+// there are stock stats and counter for updating from data model.
+// when the data model is updating, the grid and counter views make too
+
+var grid = ( function(){
+    var getCurrentime = function(){
+        var date = new Date(),
+            m = date.getMonth()+1,
+            d = date.getDate(),
+            y = date.getFullYear(),
+            t = date.toLocaleTimeString().toLowerCase();
+        return (m+"/"+d+"/"+y+"/"+t);
+    };
+
+    return {
+        refreshData: function(){
+            console.log("fetch data model");
+            console.log("Update grid component wiyh new pull data");
+        },
+        updateCounter: function(){
+            console.log("Data last updated : "+getCurrentime());
+        }
+    };
+}());
+
+// Mediator : event subscribed for observer data - grid
+var gridUpdate = function(){
+    grid.refreshData();
+    grid.updateCounter();
+};
+
+// Load Observer : observer-test.js
+var dataSubscription = pubsub.subscribe("fetchDataModel", gridUpdate);
+
+// Now we are realized there is new stock
+pubsub.publish("fetchDataModel");
