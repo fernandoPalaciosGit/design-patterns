a34aa30cf2a2519759545095540f77524421d406
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-03-16 23:29:34 +0100

    [basic-inherit] extend prototype properties by copy from superclass

--- patterns/basic-inherit.js
+++ patterns/basic-inherit.js
@@ -1,3 +1,4 @@
+// SUPERCLASS
 var Vehicle = function( settings ){
     this.distributor = settings.distributor || "distributor not prvided";
     this.aviableLicence = true;
@@ -7,18 +8,22 @@ Vehicle.prototype.enableLicence = function( enable ){
     this.aviableLicence = enable;
 };
 
+
+// SUBCLASS
 var Car = function( settings ){
     this.color = settings.color || "color not provided";
     this.model = settings.model || "model not provided";
 
     // Extending from constructor instance properties (this : distributor, aviableLicence, color, model)
-    // Extending prototype properties ( prototype : enableLicence )
     Vehicle.apply( this, arguments );
-    this.__proto__ = Vehicle.prototype;
 };
 
+// Extending prototype properties ( prototype : enableLicence )
+Car.prototype = Object.create( Vehicle.prototype );
 Car.prototype.guru = "yomismo";
 
+
+// INSTANCE Car type that extends from Vehicle
 var pegasus_21 = new Car({
     color : "yellow",
     model : "toyota pegasus X654",
