197bc82f241b0cdfa7f7e19ebdf263a0ad4facb5
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-08-11 22:33:55 +0200

    chande Test case description to pattern: Builder with factory, implement new Test case

--- platzi/test/patterns/builder/mocha.spec.CreateCarTest.js
+++ platzi/test/patterns/builder/mocha.spec.CreateCarTest.js
@@ -5,12 +5,14 @@ var _ = require('lodash'),
     expect = chai.expect,
     Car = require('./../../../main/patterns/builder/app.module.Car'),
     CarDirector = require('./../../../main/patterns/builder/app.module.CarDirector'),
-    OpelAstraBuilder = require('./../../../main/patterns/builder/app.module.CarBuilder').opelAstra,
-    NissanCascaisBuilder = require('./../../../main/patterns/builder/app.module.CarBuilder').nissanCascais;
+    CarBuilder = require('./../../../main/patterns/builder/app.module.CarBuilder'),
+    isValidBuilder = CarBuilder.isValidBuilder,
+    OpelAstraBuilder = CarBuilder.opelAstra,
+    NissanCascaisBuilder = CarBuilder.nissanCascais;
 
 describe('Design patterns', function () {
     describe('Builder with factory', function () {
-        var carDirector, opelAstraBuilder, nissanCascaisBuilder;
+        var carDirector, opelAstraBuilder, nissanCascaisBuilder, carType, opel, nissan;
 
         before(function () {
             carDirector = new CarDirector();
@@ -18,10 +20,29 @@ describe('Design patterns', function () {
             nissanCascaisBuilder = new NissanCascaisBuilder();
         });
 
-        it('should create a Opel Astra', function (next) {
-            var opel = carDirector.createCar(opelAstraBuilder);
+        it('should create an instance type defined from director builder injector', function (next) {
+            expect(isValidBuilder(opelAstraBuilder)).to.be.true;
+            expect(opelAstraBuilder).and.to.be.an.instanceof(OpelAstraBuilder);
 
-            expect(opel).to.be.an.instanceof(Car);
+            expect(isValidBuilder(nissanCascaisBuilder)).to.be.true;
+            expect(nissanCascaisBuilder).to.be.an.instanceof(NissanCascaisBuilder);
+
+            carType = carDirector.createCar(opelAstraBuilder);
+            expect(carType).to.be.an.instanceof(Car);
+
+            carType = carDirector.createCar(nissanCascaisBuilder);
+            expect(carType).to.be.an.instanceof(Car);
+            next();
+        });
+
+        it('should avoid construct instance from a non valid builders', function (next) {
+            expect(_.partial(carDirector.createCar.bind(carDirector), Object.prototype.constructor))
+                .to.throw(TypeError);
+            next();
+        });
+
+        it('should create an instance with properties from builder injector', function (next) {
+            opel = carDirector.createCar(opelAstraBuilder);
             expect(opel).to.have.property('doors', 2);
             expect(opel).to.have.property('wheels', 4);
             expect(opel).to.have.property('color', 'yellow pomello');
@@ -32,10 +53,8 @@ describe('Design patterns', function () {
             next();
         });
 
-        it('should create a Nissan Cascais', function (next) {
-            var nissan = carDirector.createCar(nissanCascaisBuilder);
-
-            expect(nissan).to.be.an.instanceof(Car);
+        it('should create an instance with properties from builder injector', function (next) {
+            nissan = carDirector.createCar(nissanCascaisBuilder);
             expect(nissan).to.have.property('doors', 5);
             expect(nissan).to.have.property('wheels', 5);
             expect(nissan).to.have.property('color', 'green inferno');
@@ -43,12 +62,6 @@ describe('Design patterns', function () {
             expect(nissan).to.have.property('cylinder', 2100);
             expect(nissan).to.have.property('HP', 170);
             expect(nissan).to.have.property('gps', '4G');
-            next();
-        });
-
-        it('should avoid contruct non valid car builders', function (next) {
-            expect(_.partial(carDirector.createCar.bind(carDirector), Object.prototype.constructor))
-                .to.throw(TypeError);
             next();
         });
     });
