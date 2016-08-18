9ef81553d2c4610637c8b9b1eba5a03388c4f335
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-08-18 20:27:26 +0200

    unit test: Decorator interface dependency injector

--- osmani-oreilly/main/patterns/decorator/app.module.object-decorator.js
+++ osmani-oreilly/main/patterns/decorator/app.module.object-decorator.js
@@ -1,60 +1,75 @@
 'use strict';
 
-var calculateCost = function (cost, perc) {
-    perc = (cost > 0) ? perc : 0;
-    var applyPerc = ((perc / 100) * cost) + cost;
+var _ = require('lodash'), cost, MacBook, engravingMacBook, insuranceLowMacBook, insuranceHighMacBook;
 
-    return function (isTaxAviable) {
-        var tax = 18,
-            applyTax = (tax / 100) * cost; // tax for devices
-        return ( !isTaxAviable ) ?
-            applyPerc : (applyPerc + applyTax);
+cost = (function () {
+    var _applyTax = 0, _changeTax, _calculateWithTax, _getCost;
+
+    _getCost = function (cost, percentage) {
+        var applyPercentage = cost > 0 ? percentage : 0;
+
+        return ((applyPercentage * cost) / 100) + cost;
     };
+
+    _changeTax = function (cost, tax) {
+        _applyTax = (tax / 100) * cost;
+    };
+
+    _calculateWithTax = function (cost, percentage, isTaxAviable) {
+        var applyCost = _getCost(cost, percentage);
+
+        return !isTaxAviable ? applyCost : applyCost + _applyTax;
+    };
+
+    return {
+        changeTax: _changeTax,
+        calculate: _.partialRight(_calculateWithTax, false),
+        calculateWithTax: _.partialRight(_calculateWithTax, true)
+    };
+}());
+
+MacBook = function (options) {
+    this.screenAviable = [11, 15, 17];
+    this.cost = options.cost || 0;
+    this.screenSize = this.checkAviableScreen(options.screenSize) ? options.screenSize : -1;
 };
 
-// MAIN CONSTRUCTOR
-var Mackbook = function (prop) {
-    prop = prop || {};
-    var screenAviable = [11, 15, 17],
-        checkAviableScreen = function (screenSize) {
-            return ( screenAviable.indexOf(screenSize) > -1 ) && screenSize;
-        };
+_.assign(MacBook.prototype, {
+    checkAviableScreen: function (screenSize) {
+        return _.includes(this.screenAviable, screenSize);
+    }
+});
 
-    this.cost = prop.cost || 0;
-    this.screenSize = checkAviableScreen(prop.screenSize) || 0;
-};
-
-// DECORATOR-1 
-var engravingMackbook = function (mackbook) {
+/**
+ * Decorator
+ */
+engravingMacBook = function (mackbook) {
     mackbook.getEngravingCost = 18;
 };
 
-// DECORATOR-2 - Overrided
-var insuranceLowMackbook = function (mackbook) {
-    var memPerc = 8.5;
-    mackbook.getInsuranceCost = calculateCost(mackbook.cost, memPerc);
+/**
+ * Decorator
+ */
+insuranceLowMacBook = function (mackbook) {
+    cost.changeTax(mackbook.cost, 20);
+    mackbook.getInsuranceCost = cost.calculateWithTax(mackbook.cost, 8.5);
 };
 
-// DECORATOR-2 - Overrided
-var insuranceHighMackbook = function (mackbook) {
-    var memPerc = 12;
-    mackbook.getInsuranceCost = calculateCost(mackbook.cost, memPerc);
+/**
+ * Decorator
+ */
+insuranceHighMacBook = function (mackbook) {
+    cost.changeTax(mackbook.cost, 18);
+    mackbook.getInsuranceCost = cost.calculateWithTax(mackbook.cost, 12);
 };
 
-var myLaptop = new Mackbook({cost: 1250, screenSize: 17});
-engravingMackbook(myLaptop); // extend this.getEngravingCost
-insuranceLowMackbook(myLaptop); // extend this.getInsuranceCost
-
-console.log(
-    '\n---WITHOUT TAX---',
-    '\nLow InsuranceCost : ' + myLaptop.getInsuranceCost());
-
-insuranceHighMackbook(myLaptop);
-console.log(
-    '\n---change decorator InsuranceCost---',
-    '\nHigh InsuranceCost : ' + myLaptop.getInsuranceCost());
-
-console.log(
-    '\n---WITH TAX---',
-    '\nEngravingCost : ' + myLaptop.getEngravingCost,
-    '\nHigh InsuranceCost with engraving tax : ' + myLaptop.getInsuranceCost(true));
+module.exports = {
+    getMacBook: function (options) {
+        return new MacBook(options || {});
+    },
+    decorateMac: {
+        engravingCost: engravingMacBook,
+        insuranceLow: insuranceLowMacBook,
+        insuranceHigh: insuranceHighMacBook
+    }
+};
--- osmani-oreilly/test/patterns/decorator/mocha.spec.object-decorator.js
+++ osmani-oreilly/test/patterns/decorator/mocha.spec.object-decorator.js
@@ -1,7 +1,42 @@
 'use strict';
-
 describe('Design patterns', function () {
-    context('Decorator interface', function () {
-        it('Structure object pattern');
+    context('Decorator interface dependency injector', function () {
+        var MacBookDecorator = require('./../../../main/patterns/decorator/app.module.object-decorator'),
+            expect = require('chai').expect,
+            getMacBook = MacBookDecorator.getMacBook,
+            engravingCost = MacBookDecorator.decorateMac.engravingCost,
+            insuranceLow = MacBookDecorator.decorateMac.insuranceLow,
+            insuranceHigh = MacBookDecorator.decorateMac.insuranceHigh;
+
+        it('should extend properties behaviour', function (next) {
+            /* jshint maxstatements:15 */
+            var myLaptop = getMacBook({cost: 1250, screenSize: 17});
+
+            expect(myLaptop).to.have.property('screenAviable')
+                .that.is.an('array')
+                .that.deep.equals([11, 15, 17]);
+            expect(myLaptop).to.have.property('cost')
+                .that.is.an('number')
+                .that.deep.equals(1250);
+            expect(myLaptop).to.have.property('screenSize')
+                .that.is.an('number')
+                .that.deep.equals(17);
+            expect(myLaptop.getInsuranceCost).not.to.exist;
+            expect(myLaptop.getEngravingCost).not.to.exist;
+
+            engravingCost(myLaptop);
+            insuranceLow(myLaptop);
+            expect(myLaptop).to.have.property('getEngravingCost')
+                .that.is.an('number')
+                .that.deep.equals(18);
+            expect(myLaptop).to.have.property('getInsuranceCost')
+                .that.is.an('number')
+                .that.deep.equals(1606.25);
+
+            insuranceHigh(myLaptop);
+            expect(myLaptop.getEngravingCost).to.equal(18);
+            expect(myLaptop.getInsuranceCost).to.equal(1625);
+            next();
+        });
     });
 });
