c56ce6384fff15237bd2f3ef9ecc3eec751cb5ff
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-09-19 23:31:15 +0200

    do not pipe  minify plugin before build module, unfix chrome debugger breakpoints

--- grunt/packages/browserify.js
+++ grunt/packages/browserify.js
@@ -27,28 +27,28 @@ module.exports = {
         .build(),
 
     'dev-test-platzi': _.cloneDeep(browserifyTestOptions)
-        .addMinifyWithSourceMap('<%= projectPaths.appPlatzi.test %>')
+        //.addMinifyWithSourceMap('<%= projectPaths.appPlatzi.test %>')
         .addOriginSource('<%= projectPaths.appPlatzi.application %>/test/**/*.js')
         .addOriginSource('!<%= projectPaths.appPlatzi.application %>/test/index.js')
         .addCompiledSource('<%= projectPaths.appPlatzi.test %>/mocha.spec.bundle.js')
         .build(),
 
     'dev-test-osmanioreilly': _.cloneDeep(browserifyTestOptions)
-        .addMinifyWithSourceMap('<%= projectPaths.appOsmaniOreilly.test %>')
+        //.addMinifyWithSourceMap('<%= projectPaths.appOsmaniOreilly.test %>')
         .addOriginSource('<%= projectPaths.appOsmaniOreilly.application %>/test/**/*.js')
         .addOriginSource('!<%= projectPaths.appOsmaniOreilly.application %>/test/index.js')
         .addCompiledSource('<%= projectPaths.appOsmaniOreilly.test %>/mocha.spec.bundle.js')
         .build(),
 
     'dev-app-widget-platzi': _.cloneDeep(browserifyAppOptions)
-        .addMinifyWithSourceMap('<%= projectPaths.appPlatzi.publicDir %>')
+        //.addMinifyWithSourceMap('<%= projectPaths.appPlatzi.publicDir %>')
         .addOriginSource('<%= projectPaths.appPlatzi.application %>/main/**/*.js')
         .addOriginSource('!<%= projectPaths.appPlatzi.application %>/main/index.js')
         .addCompiledSource('<%= projectPaths.appPlatzi.publicDir %>/app.bundle.js')
         .build(),
 
     'dev-app-widget-osmanioreilly': _.cloneDeep(browserifyAppOptions)
-        .addMinifyWithSourceMap('<%= projectPaths.appOsmaniOreilly.publicDir %>')
+        //.addMinifyWithSourceMap('<%= projectPaths.appOsmaniOreilly.publicDir %>')
         .addOriginSource('<%= projectPaths.appOsmaniOreilly.application %>/main/**/*.js')
         .addOriginSource('!<%= projectPaths.appOsmaniOreilly.application %>/main/index.js')
         .addCompiledSource('<%= projectPaths.appOsmaniOreilly.publicDir %>/app.bundle.js')
