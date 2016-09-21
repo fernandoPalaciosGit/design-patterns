84c130eb1f97f9da911c9e1813cd1f2533d72504
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-09-21 23:24:15 +0200

    test javascript prototypes, constructors and instance tricki behaviours

--- osmani-oreilly/main/patterns/happy-thoughts.js
+++ osmani-oreilly/main/patterns/happy-thoughts.js
@@ -1,143 +1,52 @@
 'use strict';
 
-// http://tobyho.com/2010/11/22/javascript-constructors-and/
+var Person, PersonFromScope, Cat, Mammal, Musician, Pianist;
 
-// Type constructor
-var Person = function (name) {
+Person = function (name) {
     this.name = name;
 };
 
-/*instances are objects type of its onstructor (self properties),
- prototype from its constructor (shared properties)*/
-var bobby = new Person('Bobby Fisher');
-bobby instanceof Person; //true
-
-// elevate this. to global scope
-/* jshint newcap:false */
-var bobby = Person('Bobby Fisher');
-bobby.name;
-/*Error, bobby.name is undefined,
- because Person() return its scope (this) to window,
- and there is not returning nothing*/
-
-window.name; // Bobby Fisher
-
-/*queremos que nuestros constructores apunten al namespace de su propio tipo,
- (in otrer words, create global variables)*/
-var Person = function (name) {
+PersonFromScope = function (name) {
     if (!(this instanceof Person)) {
         return new Person(name);
     }
 
+    this.surname = name;
+};
+
+Cat = function (name) {
     this.name = name;
 };
 
-// We can return constructor scope from any type, saving primitive types
-var Cat = function (name) {
-    this.name = name;
-};
-var Person = function (name) {
+Mammal = function (name) {
     return new Cat(name);
 };
-var bobby = Person('Bobby Fisher');
-bobby instanceof Person; // false
-bobby instanceof Cat; // true
 
-
-// PROTOTYPE INHERITANCE
-var Person = function (name) {
-    this.name = name || '';
-};
-Person.prototype.isProgrammer = true;
-
-var Pianist = function () {
-    this.handicap = 'enhancement';
-};
-Pianist.prototype = new Person(); // now pianist is a person
-Pianist.prototype.constructor = Pianist;
-Pianist.prototype.scoreSharpSound = function () {
-};
-
-var nando = new Pianist('nando');
-/* jshint proto:true */
-nando.__proto__ === Pianist.prototype; // true
-nando.constructor === Pianist.prototype.constructor; // true
-nando instanceof Pianist; // true
-
-// nando -> Pianist.prototype -> Person.prototype -> Object.prototype
-/*Each time you create a new instance of Pianist,
- we create a 3-level chain,
- in that nando is now parented by Pianist.prototype which,
- since it is an instance of Persona, is in turn parented by Person.prototype.
- We can extend properties from ever parent prototype, and fernando's going to inherit*/
-Person.prototype.surname = 'Palacios';
-Pianist.prototype.pianoMark = 'Yamaha';
-/* jshint freeze:false */
-Object.prototype.aviableInstance = true;
-
-// check instance properties
-nando.hasOwnProperty('name'); // false
-nando.hasOwnProperty('handicap'); // true
-
-// check inherit properties
-'name' in nando; // true
-'scoreSharpSound' in nando; // true
-'surname' in nando; // true
-'pianoMark' in nando; // true
-'aviableInstance' in nando; // true
-
-
-/*se puede utilizar el metodo 'apply' del prototipo de Funtion (Funtion.prototype.apply)
- en cualquier contructor para especificar (sobreescribir) la llamada al contexto 'this'*/
-var Book = function (idBook) {
-    this.ISBN = idBook;
-};
-
-Book.prototype.toStringISBN = function () { // Book -> Funtion -> Object
-    return 'That´s my book ' + this.ISBN.toUpperCase();
-};
-
-var pesca = new Book('pesca');
-var caza = new Book('caza');
-
-pesca.toStringISBN();
-caza.toStringISBN.apply(pesca); // change contex
-
-
-////////////////////////////////////////////////////////////////
-// apply arguments are arguments within function constructors //
-////////////////////////////////////////////////////////////////
-
-var CalculateFuntions = function (params) {
-    this.arrParams = params;
-};
-
-CalculateFuntions.prototype.max = function () {
-    return Math.max.apply(Math, arguments[0]);
-};
-
-var myGraph = new CalculateFuntions([2, 72, 65, 1]);
-myGraph.max(myGraph.arrParams);
-
-
-///////////////////////////
-// OVERRIDING NEW METHOD //
-///////////////////////////
-Function.prototype.new = function () {
-    var args = arguments,
-        constructor = this,
-        Fake = function () {
-            constructor.apply(this, args); // 'this' is the real constructor
-        };
-
-    Fake.prototype = constructor.prototype;
-    Fake.prototype.constructor = constructor;
-    /* jshint supernew:true */
-    return new Fake; // return the constructor object
-};
-
-var Library = function (name) {
+Musician = function (name) {
     this.name = name;
 };
-var comecocos = Library.new('comecocos');
-comecocos instanceof Library; // true
+
+Musician.prototype.isProgrammer = false;
+
+Pianist = function () {
+    this.handicap = 'enhancement';
+};
+
+Pianist.prototype = new Musician();
+Pianist.prototype.constructor = Pianist;
+Pianist.prototype.scoreSharpSound = function () {
+    return 100;
+};
+
+/* jshint freeze:false */
+Musician.prototype.surname = 'Magnificent';
+Pianist.prototype.pianoMark = 'Yamaha';
+Object.prototype.aviableInstance = true;
+
+module.exports = {
+    Person: Person,
+    PersonFromScope: PersonFromScope,
+    Cat: Cat,
+    Mammal: Mammal,
+    Pianist: Pianist
+};
--- osmani-oreilly/test/patterns/happy-thoughts.js
+++ osmani-oreilly/test/patterns/happy-thoughts.js
@@ -1,7 +1,91 @@
 'use strict';
 
 describe('Design patterns', function () {
-    context('Happy thoughts', function () {
-        it('Creational Object pattern');
+    var expect = require('chai').expect,
+        jsExamples = require('./../../main/patterns/happy-thoughts');
+
+    context('Javascript Contructors and Prototypes', function () {
+        var Person, PersonFromScope, Mammal, Cat, bobby;
+
+        before(function () {
+            Person = jsExamples.Person;
+            PersonFromScope = jsExamples.PersonFromScope;
+            Mammal = jsExamples.Mammal;
+            Cat = jsExamples.Cat;
+        });
+
+        it('should create instance from prototype', function (next) {
+            bobby = new Person('Bobby Fisher');
+            expect(bobby).to.be.instanceof(Person);
+            expect(bobby).to.have.property('name', 'Bobby Fisher');
+            next();
+        });
+
+        it('should elevate \'this\' to global scope, and could not catch instance to variable', function (next) {
+            /* jshint newcap:false */
+            bobby = Person('Bobby Fisher');
+            expect(bobby).to.not.be.instanceof(Person);
+            expect(bobby.name).to.be.undefined;
+            expect(window.name).to.be.equals('Bobby Fisher');
+            next();
+        });
+
+        it('should constructor expose scope as instance variable, neither callee constructor point outside its scope', function (next) {
+            /* jshint newcap:false */
+            bobby = PersonFromScope('Bobby Fisher');
+            expect(bobby).to.be.instanceof(PersonFromScope);
+            expect(window.surname).to.be.undefined;
+            expect(bobby.surname).to.be.equals('Bobby Fisher');
+            next();
+        });
+
+        it('should return constructor scope from any type, saving primitive types', function (next) {
+            bobby = new Mammal('Bobby Fisher');
+            expect(bobby).to.not.be.intanceof(Mammal);
+            expect(bobby).to.be.intanceof(Cat);
+            next();
+        });
+    });
+
+    context('Javascript Prototypes Inheritance', function () {
+        context('Instance Pianist -> Pianist.prototype -> Person.prototype -> Function.prototype -> Object.prototype', function () {
+            var Pianist, bobby;
+
+            before(function () {
+                Pianist = jsExamples.Pianist;
+            });
+
+            it('should inheritance prototypes properties and behaviour', function (next) {
+                /* jshint proto:true */
+                bobby = new Pianist('Grandioso');
+                expect(bobby).to.be.instanceof(Pianist);
+                expect(bobby.__proto__).to.deep.equals(Pianist.prototype);
+                expect(bobby.constructor).to.deep.equals(Pianist.prototype.constructor);
+                next();
+            });
+
+            it('should chaining inheritance with first prototype own properties', function (next) {
+                bobby = new Pianist('Grandioso');
+                expect(bobby).to.be.instanceof(Pianist);
+                expect(bobby).to.have.ownProperty('handicap');
+                expect(bobby).to.have.ownProperty('scoreSharpSound');
+                expect(bobby).to.have.ownProperty('pianoMark');
+                expect(bobby).not.to.have.ownProperty('name');
+                expect(bobby).not.to.have.ownProperty('aviableInstance');
+                next();
+            });
+
+            it('should chaining inheritance from all prototypes hierarchy', function (next) {
+                bobby = new Pianist('Grandioso');
+                expect(bobby.handicap).to.be.equals('enhancement');
+                expect(bobby.scoreSharpSound()).to.be.equals(100);
+                expect(bobby.pianoMark).to.be.equals('Yamaha');
+                expect(bobby.name).to.be.equals('Grandioso');
+                expect(bobby.surname).to.be.equals('Magnificent');
+                expect(bobby.isProgrammer).to.be.equals(false);
+                expect(bobby.aviableInstance).to.be.equals(true);
+                next();
+            });
+        });
     });
 });
