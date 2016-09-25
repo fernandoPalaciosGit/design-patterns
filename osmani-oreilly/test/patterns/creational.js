768f111c006c698b128661d381bedf433e3f34b4
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-09-25 17:06:18 +0200

    creatinal object pattern, define empty test cases

--- osmani-oreilly/test/patterns/creational.js
+++ osmani-oreilly/test/patterns/creational.js
@@ -1,7 +1,32 @@
 'use strict';
 
 describe('Design patterns', function () {
-    context('Creational', function () {
-        it('Creational object pattern');
+    context('Creational (object pattern)', function () {
+        var moduleDescriptor, objectDescriptor, TestDescriptor, test;
+
+        before(function () {
+            moduleDescriptor = require('./../../main/patterns/creational');
+            objectDescriptor = moduleDescriptor.testDataDescriptor;
+            TestDescriptor = moduleDescriptor.testObjectDataDescriptor;
+        });
+
+        beforeEach(function () {
+            test = new TestDescriptor('test descriptor');
+        });
+
+        it('should create object properties by \'data descriptor\' from \'defineProperty\' Object prototype function', function (next) {
+            objectDescriptor.someAnimal;
+            next();
+        });
+
+        it('should create writable properties', function (next) {
+            test.addSkills;
+            next();
+        });
+
+        it('should create final properties', function (next) {
+            test.addProyects;
+            next();
+        });
     });
 });
