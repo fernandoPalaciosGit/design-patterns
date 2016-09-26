ee427090a2131477d827511c7dc70255c3150b41
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-09-27 00:19:45 +0200

    build jscpd targets dynamicaly

--- grunt/packages/jscpd.js
+++ grunt/packages/jscpd.js
@@ -1,17 +1,30 @@
 'use strict';
 
-module.exports = {
-    options: {
-        'min-lines': 5,
-        'min-tokens': 70,
-        threshold: 1
-    },
-    'osmani-oreilly': {
-        path: 'osmani-oreilly',
-        files: '**/*.js'
-    },
-    'platzi': {
-        path: 'platzi',
-        files: '**/*.js'
-    }
+module.exports = function (grunt) {
+    const globJs = '**/*.js';
+    var projectPaths = grunt.config.get('projectPaths'),
+        _ = require('lodash'),
+        module = {
+            options: {
+                'min-lines': 5,
+                'min-tokens': 70,
+                threshold: 1
+            }
+        };
+
+    // grunt argument is not yet initialized at this point (load-grunt-configs), could not retrieve grunt.config.get()
+    projectPaths = !_.isEmpty(projectPaths) ? projectPaths : require('./../options/packageOptions').projectPaths;
+
+    return _.chain(projectPaths)
+        .filter(function (path) {
+            return _.isString(path.application) && !_.isEmpty(path.application);
+        })
+        .reduce(function (module, path) {
+            module[path.application] = {
+                path: path.application,
+                files: globJs
+            };
+
+            return module;
+        }, module).value();
 };
