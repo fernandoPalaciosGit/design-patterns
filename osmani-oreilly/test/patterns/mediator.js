3907f02e641a0950f80b4c12089b35c7abddc153
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-10-05 21:21:38 +0200

    test mediator object pattern

--- osmani-oreilly/test/patterns/mediator.js
+++ osmani-oreilly/test/patterns/mediator.js
@@ -1,7 +1,26 @@
 'use strict';
 
 describe('Design patterns', function () {
-    context('Mediator', function () {
-        it('Behaviour Object pattern');
+    let expect = require('chai').expect;
+
+    context('Mediator (Behaviour Object pattern)', function () {
+        let extendObserver = require('./../../main/patterns/mediator'),
+            mediator, person;
+
+        before(function () {
+            mediator = {};
+            person = null;
+            extendObserver(mediator);
+        });
+
+        it('should define a callback interface from publish/subscriber to change behaviour object', function (next) {
+            expect(person).to.be.null;
+            mediator.subscribe('setpersonalname', function (name) {
+                person = name;
+            });
+            mediator.publish('setpersonalname', 'federico');
+            expect(person).to.be.equals('federico');
+            next();
+        });
     });
 });
