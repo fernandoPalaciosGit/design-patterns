f00b9a5131a05ff09252291592bd244a050b0c97
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-10-22 18:36:19 +0200

    fix bug, cover report to implicit prototype static functions, Number.parseInt

---
+++ assessments/test/operator-statements/bestPractices.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var bestPracticesAnswers = require('./../../main/operator-statements/bestPractices').bestPracticesAnswers,
+    expect = require('chai').expect,
+    moduleScope = window || global;
+
+describe('best practices', function () {
+    it.skip('you should avoid global variables', function () {
+        bestPracticesAnswers.globals();
+        expect(moduleScope.myObject).not.to.be.ok;
+    });
+
+    it.skip('you should use parseInt correctly', function () {
+        expect(bestPracticesAnswers.parseInt('12')).to.eql(12);
+        expect(bestPracticesAnswers.parseInt('12px')).to.eql(12);
+        expect(bestPracticesAnswers.parseInt('0x12')).to.eql(0);
+    });
+
+    it.skip('you should understand strict comparison', function () {
+        expect(bestPracticesAnswers.identity(1, '1')).to.eql(false);
+        expect(bestPracticesAnswers.identity(1, 1)).to.eql(true);
+        expect(bestPracticesAnswers.identity(0, false)).to.eql(false);
+    });
+});
