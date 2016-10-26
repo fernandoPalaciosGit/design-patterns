2f6cf84b6c8675f84bb13bf15018bd2df6105b43
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-10-26 21:18:46 +0200

    flowcontrol function

--- assessments/main/operator-statements/flowControl.js
+++ assessments/main/operator-statements/flowControl.js
@@ -1,16 +1,28 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
+/**
+ * if the number is divisible by 5, the function should return 'buzz';
+ * if the number is divisible by 3, the function should return 'fizz';
+ * if the number is divisible by 3 and 5, the function should return 'fizzbuzz';
+ * otherwise the function should return the number, or false if no number
+ * was provided or the value provided is not a number
+ * */
 module.exports.flowControlAnswers = {
     fizzBuzz: function (num) {
-        // write a function that receives a number as its argument;
-        // if the number is divisible by 3, the function should return 'fizz';
-        // if the number is divisible by 5, the function should return 'buzz';
-        // if the number is divisible by 3 and 5, the function should return
-        // 'fizzbuzz';
-        //
-        // otherwise the function should return the number, or false if no number
-        // was provided or the value provided is not a number
+        var key = null,
+            codes = {
+                'fizzbuzz': num % 3 === 0 && num % 5 === 0,
+                'fizz': num % 3 === 0,
+                'buzz': num % 5 === 0,
+                'true': typeof num === 'number',
+                'false': typeof num !== 'number'
+            };
 
+        for (key in codes) {
+            if (codes.hasOwnProperty(key) && codes[key]) {
+                return key === 'true' ? num : key === 'false' ? false : key;
+            }
+        }
     }
 };
