aee0886a24febe96457b0316037768b802517bf8
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-12-06 16:36:54 +0100

    Reflect.apply

--- assessments/test/ecma2015/reflect.js
+++ assessments/test/ecma2015/reflect.js
@@ -10,6 +10,16 @@ describe('Ecma 2015 - Reflect, reflection through introspection', function () {
     });
 
     context('Reflection to override internal methods', function () {
+        it('Reflect.apply: drives method of Function#apply', function (next) {
+            let array = [7, 1, 45, 98, 3], min = 1, max = 98, type = '[object Array]';
 
+            expect(Math.max.apply(Math, array)).to.be.equals(max);
+            expect(Math.min.apply(Math, array)).to.be.equals(min);
+            expect(Object.prototype.toString.apply(array)).to.be.equals(type);
+            expect(Reflect.apply(Math.max, Math, array)).to.be.equals(max);
+            expect(Reflect.apply(Math.min, Math, array)).to.be.equals(min);
+            expect(Reflect.apply(Object.prototype.toString, array, [])).to.be.equals(type);
+            next();
+        });
     });
 });
