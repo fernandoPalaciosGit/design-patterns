eec234bcfdde7260ee36b90b660096f1e0e0cde4
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-12-06 13:31:25 +0100

    description of Reflect to access javascript engine internal methods

--- assessments/main/ecma2015/reflect.js
+++ assessments/main/ecma2015/reflect.js
@@ -8,6 +8,13 @@
  - Reflection: apply, call, bind, name, length (Function), getOwnProperties (Object)
  - Reflection / Introspection: typeof, delete, instanceof
  - Reflection through introspection - Reflect (used to discover very low level information about your code)
+
+ - Reflect to access javascript engine internal methods: [[Get]], [[Set]], [[HasOwnProperty]], [[HasOwnProperty]]
+ This mean the way to access the behaviour of the engine JS process (not to override, this is the way how to).
+ Some access on JS: Object.create [[HasOwnProperty]], getOwnPropertyNames or getOwnPropertySymbols [[HasOwnProperty]]
+ Is not handy for the usual programming development (Not tested here)
+
+ - Reflect to override javascript engine internal methods: Reflect.apply, Reflect.construct, Reflect.defineProperty...
  */
 
 module.exports = {};
--- assessments/test/ecma2015/reflect.js
+++ assessments/test/ecma2015/reflect.js
@@ -5,4 +5,11 @@ let getReflect = require('./../../main/ecma2015/reflect'),
 
 describe('Ecma 2015 - Reflect, reflection through introspection', function () {
     expect(getReflect).to.be.an('object');
+
+    context('Reflection to access internal methods: specs of JS engine', function () {
+    });
+
+    context('Reflection to override internal methods', function () {
+
+    });
 });
