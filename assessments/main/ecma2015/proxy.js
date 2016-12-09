73e0d19151fa48fc54a54a1e7ca0713cf9a545ba
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-12-09 12:15:48 +0100

    Create proxy in objects

--- assessments/main/ecma2015/proxy.js
+++ assessments/main/ecma2015/proxy.js
@@ -8,6 +8,9 @@
  - Reflection: apply, call, bind, name, length (Function), getOwnProperties (Object)
  - Reflection / Introspection: typeof, delete, instanceof
  - Reflection through intercession - Proxy (wrapping objects and intercepting their behaviours)
+
+ - Es un constructor que hereda de los prototipos que pasas por parametro e intercepta los metodos cuando este los lanza.
+ - encapsula un objeto e intercepta su comportamiento
  */
 
 module.exports = {};
--- assessments/test/ecma2015/proxy.js
+++ assessments/test/ecma2015/proxy.js
@@ -5,4 +5,45 @@ let getProxy = require('./../../main/ecma2015/proxy'),
 
 describe('Ecma 2015 - Proxy, Reflection through intercession', function () {
     expect(getProxy).to.be.an('object');
+
+    context('Create proxy in objects', function () {
+        /*todo: api polyfill not hook object*/
+        it.skip('any alterations you make with Proxy will be reflected onto the original object, and vice versa', function (next) {
+            let testObject = {
+                    [Symbol.for('foo')]: 'foo'
+                },
+                proxyTest = new Proxy(testObject, {/*handler hooks*/});
+
+            expect(testObject).not.deep.equals(proxyTest);
+            expect(testObject[Symbol.for('foo')]).to.equals(proxyTest[Symbol.for('foo')]);
+            testObject.bar = 'bar';
+            expect(proxyTest.bar).to.equals('bar');
+            next();
+        });
+
+        /*todo: api polyfill not support instanceof(Proxy)*/
+        it.skip('every one of the Reflect methods has a Proxy Handler Hook', function (next) {
+            let reflectHandlerHooks, proxyTest;
+
+            reflectHandlerHooks = {
+                apply: Reflect.apply,
+                construct: Reflect.construct,
+                defineProperty: Reflect.defineProperty,
+                getOwnPropertyDescriptor: Reflect.getOwnPropertyDescriptor,
+                deleteProperty: Reflect.deleteProperty,
+                getPrototypeOf: Reflect.getPrototypeOf,
+                setPrototypeOf: Reflect.setPrototypeOf,
+                isExtensible: Reflect.isExtensible,
+                preventExtensions: Reflect.preventExtensions,
+                get: Reflect.get,
+                set: Reflect.set,
+                has: Reflect.has,
+                ownKeys: Reflect.ownKeys
+            };
+            proxyTest = new Proxy({}, reflectHandlerHooks);
+            expect(proxyTest).to.be.an.instanceof(Proxy);
+            expect(Reflect.ownKeys(Reflect)).to.include.members(Reflect.ownKeys(reflectHandlerHooks));
+            next();
+        });
+    });
 });
